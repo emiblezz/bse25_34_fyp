a30ef9b9adffdc973784eaf547f57c23900947d1
Alemi Bless <bless.alemi@students.mak.ac.ug>
2025-03-25 17:32:57 +0300

    job seeker screens

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => AuthProvider()),
-        //ChangeNotifierProvider(create: (_) => JobProvider()),
+        ChangeNotifierProvider(create: (_) => JobProvider()),
         //ChangeNotifierProvider(create: (_) => NotificationProvider()),
       ],
       child: MaterialApp(
